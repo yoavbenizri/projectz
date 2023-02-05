a81ed105a3b504e1bec7821fcff1b8557de980c4
Amir Rave <66911150+amirave@users.noreply.github.com>
2023-02-05 19:08:48 +0200

    Init React and basic chat functionality

---
+++ projectz-client/src/MessageInput.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import Message from './Props';
+
+interface Props {
+    onMessageSubmit: (user: string, text: string, self : boolean) => void;
+}
+
+const MessageInput: React.FC<Props> = ({ onMessageSubmit }) => {
+  const [user, setUser] = useState('');
+  const [text, setText] = useState('');
+  const [self, setSelf] = useState(false);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onMessageSubmit(user, text, self);
+    setText('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Username" value={user} onChange={e => setUser(e.target.value)} required={true}/>
+      <input type="text" placeholder="Message" value={text} onChange={e => setText(e.target.value)} required={true}/>
+      <input type="checkbox" onChange={e => setSelf(!self)}></input>
+      <button type="submit">Send</button>
+    </form>
+  );
+};
+
+export default MessageInput;
